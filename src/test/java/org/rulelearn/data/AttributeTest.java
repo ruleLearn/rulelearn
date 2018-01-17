7950834fd3c457eb355b92cd4f8799c044734737
Jurek Blaszczynski <jurek.blaszczynski@cs.put.poznan.pl>
2018-01-17 15:44:48 +0100

    Added test for Attribute with PairField-type values.

--- src/test/java/org/rulelearn/data/AttributeTest.java
+++ src/test/java/org/rulelearn/data/AttributeTest.java
@@ -20,8 +20,12 @@ import java.security.NoSuchAlgorithmException;
 
 import org.junit.jupiter.api.Test;
 import org.rulelearn.types.ElementList;
+import org.rulelearn.types.EnumerationField;
 import org.rulelearn.types.EnumerationFieldFactory;
+import org.rulelearn.types.IntegerField;
 import org.rulelearn.types.IntegerFieldFactory;
+import org.rulelearn.types.PairField;
+import org.rulelearn.types.RealField;
 import org.rulelearn.types.RealFieldFactory;
 import org.rulelearn.types.UnknownSimpleFieldMV2;
 
@@ -44,8 +48,8 @@ public class AttributeTest {
 		Attribute[] attributes = new Attribute[5];
 		
 		//TODO Should be default value taken from a config class
-		Attribute attribute = new Attribute("a1", true, AttributeType.CONDITION, IntegerFieldFactory.getInstance().create(0, AttributePreferenceType.GAIN), 
-				new UnknownSimpleFieldMV2(), AttributePreferenceType.GAIN);
+		Attribute attribute = new Attribute("a1", true, AttributeType.CONDITION, IntegerFieldFactory.getInstance().create(0, AttributePreferenceType.NONE), 
+				new UnknownSimpleFieldMV2(), AttributePreferenceType.NONE);
 		attributes[0] = attribute;
 		attribute = new Attribute("c1", true, AttributeType.CONDITION, RealFieldFactory.getInstance().create(0, AttributePreferenceType.GAIN),
 				new UnknownSimpleFieldMV2(), AttributePreferenceType.GAIN);
@@ -64,6 +68,40 @@ public class AttributeTest {
 			attribute = new Attribute("d", true, AttributeType.DECISION, EnumerationFieldFactory.getInstance().create(new ElementList(labels), 0, AttributePreferenceType.GAIN),
 					new UnknownSimpleFieldMV2(), AttributePreferenceType.GAIN);
 			attributes[4] = attribute;
+		}
+		catch (NoSuchAlgorithmException ex) {
+			System.out.println(ex);
+		}
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(attributes);
+		System.out.println(json);
+		
+	}
+	
+	/**
+	 * Tests construction and serialization to JSON
+	 */
+	@Test
+	public void testConstruction02() {
+		Attribute[] attributes = new Attribute[3];
+		//TODO Should be default value taken from a config class
+		Attribute attribute = new Attribute("c1", true, AttributeType.CONDITION, 
+				new PairField<IntegerField>(IntegerFieldFactory.getInstance().create(0, AttributePreferenceType.GAIN), IntegerFieldFactory.getInstance().create(1, AttributePreferenceType.GAIN)), 
+				new UnknownSimpleFieldMV2(), AttributePreferenceType.GAIN);
+		attributes[0] = attribute;
+		attribute = new Attribute("c2", true, AttributeType.CONDITION, 
+				new PairField<RealField>(RealFieldFactory.getInstance().create(0, AttributePreferenceType.GAIN), RealFieldFactory.getInstance().create(1, AttributePreferenceType.GAIN)), 
+				new UnknownSimpleFieldMV2(), AttributePreferenceType.GAIN);
+		attributes[1] = attribute;
+		
+		try {
+			String [] values = {"0", "1", "2"}; 
+			attribute = new Attribute("d1", true, AttributeType.CONDITION, 
+					new PairField<EnumerationField>(EnumerationFieldFactory.getInstance().create(new ElementList(values), 0, AttributePreferenceType.GAIN), 
+							EnumerationFieldFactory.getInstance().create(new ElementList(values), 1, AttributePreferenceType.GAIN)), 
+					new UnknownSimpleFieldMV2(), AttributePreferenceType.GAIN);
+			attributes[2] = attribute;
 		}
 		catch (NoSuchAlgorithmException ex) {
 			System.out.println(ex);
