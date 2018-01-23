f8cff3e7b9627813454666639500dd8b1776116d
Marcin Szelag <marcin.szelag@cs.put.poznan.pl>
2018-01-23 01:31:44 +0100

    Written unit tests for class UnknownSimpleFieldMV2.

--- src/test/java/org/rulelearn/types/UnknownSimpleFieldMV2Test.java
+++ src/test/java/org/rulelearn/types/UnknownSimpleFieldMV2Test.java
@@ -19,6 +19,7 @@ package org.rulelearn.types;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.rulelearn.core.TernaryLogicValue;
+import org.rulelearn.core.UncomparableException;
 import org.rulelearn.data.AttributePreferenceType;
 
 /**
@@ -29,6 +30,9 @@ import org.rulelearn.data.AttributePreferenceType;
  */
 class UnknownSimpleFieldMV2Test {
 
+	/**
+	 * Test for {@link UnknownSimpleFieldMV2#isAtLeastAsGoodAs(Field)} method. Tests is mv_2 missing value outranks the same missing value.
+	 */
 	@Test
 	public void testIsAtLeastAsGoodAs_01() {
 		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
@@ -36,6 +40,9 @@ class UnknownSimpleFieldMV2Test {
 		assertEquals(mvField.isAtLeastAsGoodAs(otherField), TernaryLogicValue.TRUE);
 	}
 	
+	/**
+	 * Test for {@link UnknownSimpleFieldMV2#isAtLeastAsGoodAs(Field)} method. Tests is mv_2 missing value outranks a known value.
+	 */
 	@Test
 	public void testIsAtLeastAsGoodAs_02() {
 		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
@@ -43,6 +50,9 @@ class UnknownSimpleFieldMV2Test {
 		assertEquals(mvField.isAtLeastAsGoodAs(otherField), TernaryLogicValue.TRUE);
 	}
 	
+	/**
+	 * Test for {@link UnknownSimpleFieldMV2#isAtLeastAsGoodAs(Field)} method. Tests is mv_2 missing value outranks a known value.
+	 */
 	@Test
 	public void testIsAtLeastAsGoodAs_03() {
 		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
@@ -50,6 +60,9 @@ class UnknownSimpleFieldMV2Test {
 		assertEquals(mvField.isAtLeastAsGoodAs(otherField), TernaryLogicValue.TRUE);
 	}
 	
+	/**
+	 * Test for {@link UnknownSimpleFieldMV2#isAtLeastAsGoodAs(Field)} method. Tests is mv_2 missing value outranks a known value.
+	 */
 	@Test
 	public void testIsAtLeastAsGoodAs_04() {
 		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
@@ -61,45 +74,172 @@ class UnknownSimpleFieldMV2Test {
 		}
 		assertEquals(mvField.isAtLeastAsGoodAs(otherField), TernaryLogicValue.TRUE);
 	}
+	
+	/**
+	 * Test for {@link UnknownSimpleFieldMV2#isAtMostAsGoodAs(Field)} method. Tests is mv_2 missing value is outranked by the same missing value.
+	 */
+	@Test
+	public void testIsAtMostAsGoodAs_01() {
+		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
+		Field otherField = new UnknownSimpleFieldMV2();
+		assertEquals(mvField.isAtMostAsGoodAs(otherField), TernaryLogicValue.TRUE);
+	}
+	
+	/**
+	 * Test for {@link UnknownSimpleFieldMV2#isAtMostAsGoodAs(Field)} method. Tests is mv_2 missing value is outranked by a known value.
+	 */
+	@Test
+	public void testIsAtMostAsGoodAs_02() {
+		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
+		Field otherField = IntegerFieldFactory.getInstance().create(3, AttributePreferenceType.GAIN);
+		assertEquals(mvField.isAtMostAsGoodAs(otherField), TernaryLogicValue.TRUE);
+	}
+	
+	/**
+	 * Test for {@link UnknownSimpleFieldMV2#isAtMostAsGoodAs(Field)} method. Tests is mv_2 missing value is outranked by a known value.
+	 */
+	@Test
+	public void testIsAtMostAsGoodAs_03() {
+		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
+		Field otherField = RealFieldFactory.getInstance().create(-1.0, AttributePreferenceType.COST);
+		assertEquals(mvField.isAtMostAsGoodAs(otherField), TernaryLogicValue.TRUE);
+	}
+	
+	/**
+	 * Test for {@link UnknownSimpleFieldMV2#isAtMostAsGoodAs(Field)} method. Tests is mv_2 missing value is outranked by a known value.
+	 */
+	@Test
+	public void testIsAtMostAsGoodAs_04() {
+		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
+		Field otherField = null;
+		try {
+			otherField = EnumerationFieldFactory.getInstance().create(new ElementList(new String[] {"a", "b", "c"}), 1, AttributePreferenceType.NONE);
+		} catch (Exception exception) {
+			fail("Element list could not be constructed.");
+		}
+		assertEquals(mvField.isAtMostAsGoodAs(otherField), TernaryLogicValue.TRUE);
+	}
+	
+	/**
+	 * Test for {@link UnknownSimpleFieldMV2#isEqualTo(Field)} method. Tests is mv_2 missing value is equal to the same missing value.
+	 */
+	@Test
+	public void testIsEqualTo_01() {
+		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
+		Field otherField = new UnknownSimpleFieldMV2();
+		assertEquals(mvField.isEqualTo(otherField), TernaryLogicValue.TRUE);
+	}
+	
+	/**
+	 * Test for {@link UnknownSimpleFieldMV2#isEqualTo(Field)} method. Tests is mv_2 missing value is equal to a known value.
+	 */
+	@Test
+	public void testisEqualTo_02() {
+		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
+		Field otherField = IntegerFieldFactory.getInstance().create(3, AttributePreferenceType.GAIN);
+		assertEquals(mvField.isEqualTo(otherField), TernaryLogicValue.TRUE);
+	}
+	
+	/**
+	 * Test for {@link UnknownSimpleFieldMV2#isEqualTo(Field)} method. Tests is mv_2 missing value is equal to a known value.
+	 */
+	@Test
+	public void testisEqualTo_03() {
+		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
+		Field otherField = RealFieldFactory.getInstance().create(-1.0, AttributePreferenceType.COST);
+		assertEquals(mvField.isEqualTo(otherField), TernaryLogicValue.TRUE);
+	}
+	
+	/**
+	 * Test for {@link UnknownSimpleFieldMV2#isEqualTo(Field)} method. Tests is mv_2 missing value is equal to a known value.
+	 */
+	@Test
+	public void testisEqualTo_04() {
+		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
+		Field otherField = null;
+		try {
+			otherField = EnumerationFieldFactory.getInstance().create(new ElementList(new String[] {"a", "b", "c"}), 1, AttributePreferenceType.NONE);
+		} catch (Exception exception) {
+			fail("Element list could not be constructed.");
+		}
+		assertEquals(mvField.isEqualTo(otherField), TernaryLogicValue.TRUE);
+	}
+	
+	/**
+	 * Test for {@link UnknownSimpleFieldMV2#reverseIsAtLeastAsGoodAs(KnownSimpleField)} method. Tests is other known field outranks mv_2 missing value.
+	 */
+	@Test
+	public void testReverseIsAtLeastAsGoodAs_01() {
+		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
+		KnownSimpleField otherField = IntegerFieldFactory.getInstance().create(1, AttributePreferenceType.GAIN);
+		assertEquals(mvField.reverseIsAtLeastAsGoodAs(otherField), TernaryLogicValue.TRUE);
+	}
+	
+	/**
+	 * Test for {@link UnknownSimpleFieldMV2#reverseIsAtMostAsGoodAs(KnownSimpleField)} method. Tests is other known field is outranked by mv_2 missing value.
+	 */
+	@Test
+	public void testReverseIsAtMostAsGoodAs_01() {
+		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
+		KnownSimpleField otherField = RealFieldFactory.getInstance().create(1, AttributePreferenceType.COST);
+		assertEquals(mvField.reverseIsAtMostAsGoodAs(otherField), TernaryLogicValue.TRUE);
+	}
 
-//	@Test
-//	public void testIsAtMostAsGoodAs() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testIsEqualTo() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testReverseIsAtLeastAsGoodAs() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testReverseIsAtMostAsGoodAs() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testReverseIsEqualTo() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testCompareToEx() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testReverseCompareToEx() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSelfClone() {
-//		fail("Not yet implemented");
-//	}
+	/**
+	 * Test for {@link UnknownSimpleFieldMV2#reverseIsEqualTo(KnownSimpleField)} method. Tests is other known field is equal to mv_2 missing value.
+	 */
+	@Test
+	public void testReverseIsEqualTo_01() {
+		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
+		KnownSimpleField otherField = null;
+		
+		try {
+			otherField = EnumerationFieldFactory.getInstance().create(new ElementList(new String[] {"a", "b", "c"}), 1, AttributePreferenceType.NONE);
+		} catch (Exception exception) {
+			fail("Element list could not be constructed.");
+		}
+		assertEquals(mvField.reverseIsEqualTo(otherField), TernaryLogicValue.TRUE);
+	}
+	
+	/**
+	 * Test for {@link UnknownSimpleFieldMV2#compareToEx(Field)} method. Tests is result of comparison of mv_2 missing value and another mv_2 missing value is equal to zero.
+	 */
+	@Test
+	public void testCompareToEx_01() throws UncomparableException {
+		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
+		Field otherField = new UnknownSimpleFieldMV2();
+		assertEquals(mvField.compareToEx(otherField), 0);
+	}
+
+	/**
+	 * Test for {@link UnknownSimpleFieldMV2#compareToEx(Field)} method. Tests is result of comparison of mv_2 missing value and other known field is equal to zero.
+	 */
+	@Test
+	public void testCompareToEx_02() throws UncomparableException {
+		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
+		KnownSimpleField otherField = IntegerFieldFactory.getInstance().create(2, AttributePreferenceType.GAIN);
+		assertEquals(mvField.compareToEx(otherField), 0);
+	}
+
+	/**
+	 *  Test for {@link UnknownSimpleFieldMV2#reverseCompareToEx(Field)} method. Tests is result of comparison of a known simple field and mv_2 missing value is equal to zero.
+	 */
+	@Test
+	public void testReverseCompareToEx() throws UncomparableException {
+		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
+		KnownSimpleField otherField = RealFieldFactory.getInstance().create(-1.0, AttributePreferenceType.NONE);
+		assertEquals(mvField.reverseCompareToEx(otherField), 0);
+	}
+
+	/**
+	 * Test for {@link UnknownSimpleFieldMV2#selfClone()} method.
+	 */
+	@Test
+	public void testSelfClone() {
+		UnknownSimpleField mvField = new UnknownSimpleFieldMV2();
+		Field clonedField = mvField.selfClone();
+		
+		assertTrue(clonedField instanceof UnknownSimpleFieldMV2);
+	}
 
 }
