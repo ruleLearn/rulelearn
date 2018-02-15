a42d017df14f4be26b207127088dbd4065cff009
Jurek Blaszczynski <jurek.blaszczynski@cs.put.poznan.pl>
2018-02-15 12:38:28 +0100

    InformationTableBuilder extended and basic jUnit tests added.

--- src/main/java/org/rulelearn/data/InformationTableBuilder.java
+++ src/main/java/org/rulelearn/data/InformationTableBuilder.java
@@ -18,9 +18,20 @@ package org.rulelearn.data;
 
 import java.util.List;
 import org.rulelearn.data.json.AttributeDeserializer;
+import org.rulelearn.types.ElementList;
+import org.rulelearn.types.EnumerationField;
+import org.rulelearn.types.EnumerationFieldFactory;
 import org.rulelearn.types.Field;
+import org.rulelearn.types.IntegerField;
+import org.rulelearn.types.IntegerFieldFactory;
+import org.rulelearn.types.RealField;
+import org.rulelearn.types.RealFieldFactory;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.univocity.parsers.conversions.TrimConversion;
+
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 
 /**
  * Builder for {@link InformationTable}. Allows building of an information table by setting attributes first, and then iteratively adding objects (rows).
@@ -30,84 +41,228 @@ import com.google.gson.GsonBuilder;
  * @author Marcin Szeląg (<a href="mailto:marcin.szelag@cs.put.poznan.pl">marcin.szelag@cs.put.poznan.pl</a>)
  */
 public class InformationTableBuilder {
+	/** 
+	 * Default string representations of missing value.
+	 */
+	protected final static String [] DEFAULT_MISSING_VALUE_STRINGS = {"?", "*", "NA"};
 	
 	/**
 	 * All attributes of the constructed information table.
 	 */
 	protected Attribute[] attributes = null;
+	
 	/**
 	 * List of fields of subsequent objects of constructed information table; each array contains subsequent fields of a single object (row) in the information table.
 	 */
 	protected List<Field[]> fields = null;
-
+	
 	/**
-	 * Sole constructor initializing this builder.
+	 * Array of string representations of missing values.
 	 */
-	public InformationTableBuilder() {
-		this.attributes = null;
-		this.fields = null;
+	protected String [] missingValueStrings = null;
+	
+	/**
+	 * Separator of values in string representation of information.
+	 */
+	protected String separator = null;
+	
+	/**
+	 * Converter which removes leading and trailing white spaces from an input {@link String}.
+	 */
+	TrimConversion trimConversion = null;
+	
+	/**
+	 * Default constructor initializing this information table builder.
+	 */
+	protected InformationTableBuilder() {
+		this.missingValueStrings = InformationTableBuilder.DEFAULT_MISSING_VALUE_STRINGS;
+		this.trimConversion = new TrimConversion();
 	}
 	
 	/**
-	 * Sets attributes of the constructed information table. This should be done first, before adding any objects.
+	 * Constructor initializing this information table builder and setting attributes.
+	 * 
+	 * @param attributes table with attributes
+	 * @throws NullPointerException if all or some of attributes of the constructed information table have not been set
+	 */
+	public InformationTableBuilder(Attribute[] attributes) {
+		this();
+		
+		this.attributes = attributes;
+		
+		// check attributes and initialize fields
+		if (attributes != null) {
+			for (Attribute attribute : attributes)
+				if (attribute == null) throw new NullPointerException("At least one attribute is not set");
+			this.fields = new ObjectArrayList<Field []>();
+		}
+		else
+			throw new NullPointerException("Attributes are not set");
+	}
+	
+	/**
+	 * Constructor initializing this information table builder and setting attributes.
+	 * 
+	 * @param attributes table with attributes
+	 * @param separator separator of object's evaluations
+	 * @throws NullPointerException if all or some of attributes of the constructed information table have not been set
+	 */
+	public InformationTableBuilder(Attribute[] attributes, String separator) {
+		this(attributes);
+		this.separator = separator;
+	}
+	
+	/**
+	 * Constructor initializing this information table builder and setting attributes. It requires definition of attributes in JSON.
 	 * 
 	 * @param jsonAttributes JSON string encoding information about all attributes
+	 * @throws NullPointerException if all or some of attributes of the constructed information table have not been set
 	 */
-	public void setAttributesFromJSON(String jsonAttributes) {
+	public InformationTableBuilder(String jsonAttributes) {
+		this();
+		
+		// parse attributes
 		GsonBuilder gsonBuilder = new GsonBuilder();
 		gsonBuilder.registerTypeAdapter(Attribute.class, new AttributeDeserializer());
-		//gsonBuilder.registerTypeAdapter(Attribute.class, new AttributeSerializer());
 		Gson gson = gsonBuilder.create();
-		
 		this.attributes = gson.fromJson(jsonAttributes, Attribute[].class);
+		
+		// check attributes and initialize fields
+		if (attributes != null) {
+			for (Attribute attribute : attributes)
+				if (attribute == null) throw new NullPointerException("At least one attribute is not set");
+			this.fields = new ObjectArrayList<Field []>();
+		}
+		else
+			throw new NullPointerException("Attributes are not set");
 	}
 	
 	/**
-	 * Adds one object to this builder. Assumes that attributes have already been set.
+	 * Constructor initializing this information table builder and setting attributes. It requires definition of attributes in JSON.
+	 * 
+	 * @param jsonAttributes JSON string encoding information about all attributes
+	 * @param separator separator of object's evaluations
+	 * @throws NullPointerException if all or some of attributes of the constructed information table have not been set
+	 */
+	public InformationTableBuilder(String jsonAttributes, String separator) {
+		this(jsonAttributes);
+		this.separator = separator;
+	}
+	
+	/**
+	 * Adds one object to this builder. 
+	 * Given string is considered to contain subsequent evaluations of a single object, separated by the {@link InformationTableBuilder#separator}.
+	 * 
+	 * @param objectEvaluations string with object's evaluations, separated by the given separator
+	 */
+	public void addObject(String objectEvaluations) {
+		if (this.separator != null)
+			addObject(objectEvaluations.split(this.separator));
+	}
+	
+	/**
+	 * Adds one object to this builder. 
 	 * Given string is considered to contain subsequent evaluations of a single object, separated by the given separator.
 	 * 
-	 * @param objectFields string with object's evaluations, separated by the given separator
+	 * @param objectEvaluations string with object's evaluations, separated by the given separator
 	 * @param separator separator of object's evaluations
 	 */
-	public void addObject(String objectFields, String separator) {
-		addObject(objectFields.split(separator));
+	public void addObject(String objectEvaluations, String separator) {
+		if (separator != null)
+			addObject(objectEvaluations.split(separator));
 	}
 	
 	/**
-	 * Adds one object to this builder. Assumes that attributes have already been set.
+	 * Adds one object to this builder. 
 	 * Given array is considered to contain subsequent evaluations of a single object.
 	 * 
-	 * @param objectFields single object's evaluations
+	 * @param objectEvaluations single object's evaluations
+	 * 
+	 * @throws IndexOutOfBoundsException if given attribute index does not correspond to any attribute of the constructed information table
 	 */
-	public void addObject(String[] objectFields) {
-		Field[] object = new Field[objectFields.length];
+	public void addObject(String[] objectEvaluations) {
+		Field[] object = new Field[objectEvaluations.length];
+		if (objectEvaluations.length > attributes.length)
+			throw new IndexOutOfBoundsException("Object has more evaluations than the number of attributes declared.");
 		
-		for (int i = 0; i < objectFields.length; i++) {
-			object[i] = parseField(objectFields[i].trim(), i);
+		for (int i = 0; i < objectEvaluations.length; i++) {
+			object[i] = parseEvaluation(objectEvaluations[i], attributes[i]);
 		}
 		
 		this.fields.add(object);
 	}
 	
 	/**
-	 * Parses one object's evaluation (field).
+	 * Parses one object's evaluation and transforms it into a field {@link Field}.
 	 * 
-	 * @param field text-encoded object's evaluation
-	 * @param attributeIndex index of the attribute whose value should be parsed
+	 * @param evaluation text-encoded object's evaluation
+	 * @param attribute whose value should be parsed
 	 * 
 	 * @return constructed field
-	 * 
-	 * @throws NullPointerException if attributes of the constructed information table have not been already set
-	 * @throws IndexOutOfBoundsException if given attribute index does not correspond to any attribute of the constructed information table
+	 * @throws NumberFormatException if evaluation of an numeric attribute can't be parsed
+	 * @throws IndexOutOfBoundsException if evaluation of an enumeration attribute can't be parsed  
 	 */
-	protected Field parseField(String field, int attributeIndex) {
-//		switch(attributes[attributeIndex].valueType) {
-//		 
-//		}
-		//TODO: implement parsing of fields, taking into account also missing values
-		return null;
-	}
+	protected Field parseEvaluation(String evaluation, Attribute attribute) throws NumberFormatException, IndexOutOfBoundsException {
+		Field field = null;
+		boolean missingValue = false;
 	
+		
+		// get rid of white spaces
+		evaluation = trimConversion.execute(evaluation);
+		// check whether it is a missing value
+		for (String missingValueString : this.missingValueStrings) {
+			if (missingValueString.equalsIgnoreCase(evaluation)) {
+				missingValue = true;
+				break;
+			}
+		}
+		
+		if (!missingValue) {
+			Field valueType = attribute.getValueType(); 
+			if (valueType instanceof IntegerField) {
+				try {
+					field = IntegerFieldFactory.getInstance().create(Integer.parseInt(evaluation), attribute.preferenceType);
+				}
+				catch (NumberFormatException ex) {
+					// just assign a reference (no new copy of missing value field is made)
+					field = attribute.getMissingValueType();
+					throw new NumberFormatException(ex.getMessage());
+				}
+			}
+			else if (valueType instanceof RealField) {
+				try {
+					field = RealFieldFactory.getInstance().create(Double.parseDouble(evaluation), attribute.preferenceType);
+				}
+				catch (NumberFormatException ex) {
+					// just assign a reference (no new copy of missing value field is made)
+					field = attribute.getMissingValueType();
+					throw new NumberFormatException(ex.getMessage());
+				}
+			}
+			else if (valueType instanceof EnumerationField) {
+				// TODO some optimization is needed here (e.g., construction of a table with element lists)
+				int index = ((EnumerationField)valueType).getElementList().getIndex(evaluation);
+				if (index != ElementList.DEFAULT_INDEX)
+					field = EnumerationFieldFactory.getInstance().create(((EnumerationField)valueType).getElementList(), index, attribute.preferenceType);
+				else {
+					field = attribute.getMissingValueType();
+					throw new IndexOutOfBoundsException(new String("Incorrect value of enumeration: ").concat(evaluation));
+				}
+					
+			}
+			else {
+				// just assign a reference (no new copy of missing value field is made) 
+				field = attribute.getMissingValueType();
+			}
+		}
+		else {
+			// just assign a reference (no new copy of missing value field is made) 
+			field = attribute.getMissingValueType();
+		}
+		
+		return field;
+	}
+		
 	/**
 	 * Builds information table.
 	 * 
@@ -115,6 +270,20 @@ public class InformationTableBuilder {
 	 */
 	public InformationTable build() {
 		return new InformationTable(attributes, fields);
+	}
+
+	/**
+	 * @return the missingValueStrings
+	 */
+	public String[] getMissingValueStrings() {
+		return missingValueStrings;
+	}
+
+	/**
+	 * @param missingValueStrings the missingValueStrings to set
+	 */
+	public void setMissingValueStrings(String[] missingValueStrings) {
+		this.missingValueStrings = missingValueStrings;
 	}
 	
 }
---
+++ src/test/java/org/rulelearn/data/InformationTableBuilderTest.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (C) Jerzy Błaszczyński, Marcin Szeląg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.rulelearn.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.rulelearn.core.TernaryLogicValue;
+import org.rulelearn.types.EnumerationField;
+import org.rulelearn.types.EnumerationFieldFactory;
+import org.rulelearn.types.IntegerFieldFactory;
+import org.rulelearn.types.RealFieldFactory;
+
+
+/**
+ * InformationTableBuilderTest
+ *
+ * @author Jerzy Błaszczyński (<a href="mailto:jurek.blaszczynski@cs.put.poznan.pl">jurek.blaszczynski@cs.put.poznan.pl</a>)
+ * @author Marcin Szeląg (<a href="mailto:marcin.szelag@cs.put.poznan.pl">marcin.szelag@cs.put.poznan.pl</a>)
+ *
+ */
+class InformationTableBuilderTest {
+	
+	private String jsonAttributes1 = "[" + 
+			 "{" +
+			    "\"name\": \"a1\"," +
+			    "\"active\": true," +
+			    "\"preferenceType\": \"none\"," +
+			    "\"type\": \"condition\"," +
+			    "\"valueType\": \"integer\"," +
+			    "\"missingValueType\": \"mv2\"" +
+			  "}," +
+			  "{" +
+			    "\"name\": \"c1\"," +
+			    "\"active\": true," +
+			    "\"preferenceType\": \"cost\"," +
+			    "\"type\": \"condition\"," +
+			    "\"valueType\": \"real\"," +
+			    "\"missingValueType\": \"mv2\"" +
+			  "}," +
+			  "{" +
+			    "\"name\": \"d1\"," +
+			    "\"active\": true," +
+			    "\"preferenceType\": \"gain\"," +
+			    "\"type\": \"decision\"," +
+			    "\"valueType\": \"enumeration\"," +
+			    "\"domain\": [\"a\",\"b\",\"c\"]," +
+			    "\"missingValueType\": \"mv2\"" +
+			  "}" +
+		"]";
+	
+	/**
+	 * Test method for {@link org.rulelearn.data.InformationTableBuilder#setAttributesFromJSON(java.lang.String)}.
+	 */
+	@Test
+	void testConstructionOfInformationTableBuilder() {		 
+		InformationTableBuilder iTB = new InformationTableBuilder(jsonAttributes1);
+		assertTrue(iTB != null);
+	}
+
+	/**
+	 * Test method for {@link org.rulelearn.data.InformationTableBuilder#addObject(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	void testAddObject() {
+		InformationTableBuilder iTB = new InformationTableBuilder(jsonAttributes1, ",");
+		iTB.addObject("1, 1.0, a");
+		iTB.addObject("2, 2.0, b");
+		InformationTable iT = iTB.build();
+		assertTrue(iT.getField(0, 0).isEqualTo(IntegerFieldFactory.getInstance().create(1, AttributePreferenceType.NONE)) == TernaryLogicValue.TRUE);
+		assertTrue(iT.getField(0, 1).isEqualTo(RealFieldFactory.getInstance().create(1.0, AttributePreferenceType.COST)) == TernaryLogicValue.TRUE);
+		assertTrue(iT.getField(1, 2).isEqualTo(EnumerationFieldFactory.getInstance().create(((EnumerationField)iT.getAttributes()[2].getValueType()).getElementList(), 
+												1, AttributePreferenceType.GAIN)) == TernaryLogicValue.TRUE);
+	}
+
+}
