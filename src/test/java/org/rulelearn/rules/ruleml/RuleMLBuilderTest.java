b0c1e130975dae59111bd6fc13b52b80d5d13e95
Jurek Blaszczynski <jurek.blaszczynski@cs.put.poznan.pl>
2018-07-17 22:34:15 +0200

    Added tests for construction of RuleML representation of rule by RuleMLBuilder.

--- src/test/java/org/rulelearn/rules/ruleml/RuleMLBuilderTest.java
+++ src/test/java/org/rulelearn/rules/ruleml/RuleMLBuilderTest.java
@@ -14,6 +14,8 @@ import org.rulelearn.data.AttributePreferenceType;
 import org.rulelearn.data.AttributeType;
 import org.rulelearn.data.EvaluationAttribute;
 import org.rulelearn.data.EvaluationAttributeWithContext;
+import org.rulelearn.rules.Condition;
+import org.rulelearn.rules.Rule;
 import org.rulelearn.rules.RuleSemantics;
 import org.rulelearn.rules.RuleSet;
 import org.rulelearn.rules.SimpleConditionAtLeast;
@@ -22,6 +24,7 @@ import org.rulelearn.rules.SimpleConditionEqual;
 import org.rulelearn.types.ElementList;
 import org.rulelearn.types.EnumerationField;
 import org.rulelearn.types.EnumerationFieldFactory;
+import org.rulelearn.types.EvaluationField;
 import org.rulelearn.types.IntegerField;
 import org.rulelearn.types.IntegerFieldFactory;
 import org.rulelearn.types.RealField;
@@ -51,7 +54,9 @@ class RuleMLBuilderTest {
 	private SimpleConditionAtLeast decisionAtLeast;
 	@Mock
 	private SimpleConditionAtMost decisionAtMost;
-
+	
+	@Mock
+	private Rule rule;
 	
 	private String conditionAtLeastRuleML;
 	private String conditionAtMostRuleML;
@@ -182,21 +187,104 @@ class RuleMLBuilderTest {
 				"<ind>a</ind>\n" + 
 				"\t<var>cd1</var>\n" + 
 				"</atom>\n";
-		
 	}
 	
 	/**
 	 * Test method for {@link org.rulelearn.rules.ruleml.RuleMLBuilder#toRuleMLString(org.rulelearn.rules.Condition, int)}.
 	 */
 	@Test
-	void testRuleMLBuilderCondition () {
-		//System.out.println(ruleMLBuilder.toRuleMLString(conditionAtLeast, 0));
-		assertEquals(ruleMLBuilder.toRuleMLString(conditionAtLeast, 0), this.conditionAtLeastRuleML);
-		assertEquals(ruleMLBuilder.toRuleMLString(conditionAtMost, 0), this.conditionAtMostRuleML);
-		assertEquals(ruleMLBuilder.toRuleMLString(conditionEqual, 0), this.conditionEqualRuleML);
-		//System.out.println(ruleMLBuilder.toRuleMLString(decisionAtLeast, 0));
-		assertEquals(ruleMLBuilder.toRuleMLString(decisionAtLeast, 0), this.decisionAtLeastRuleML);
-		assertEquals(ruleMLBuilder.toRuleMLString(decisionAtMost, 0), this.decisionAtMostRuleML);
+	void testRuleMLBuilderCondition() {
+		//System.out.println(ruleMLBuilder.toRuleMLString(this.conditionAtLeast, 0));
+		assertEquals(ruleMLBuilder.toRuleMLString(this.conditionAtLeast, 0), this.conditionAtLeastRuleML);
+		assertEquals(ruleMLBuilder.toRuleMLString(this.conditionAtMost, 0), this.conditionAtMostRuleML);
+		assertEquals(ruleMLBuilder.toRuleMLString(this.conditionEqual, 0), this.conditionEqualRuleML);
+		//System.out.println(ruleMLBuilder.toRuleMLString(this.decisionAtLeast, 0));
+		assertEquals(ruleMLBuilder.toRuleMLString(this.decisionAtLeast, 0), this.decisionAtLeastRuleML);
+		assertEquals(ruleMLBuilder.toRuleMLString(this.decisionAtMost, 0), this.decisionAtMostRuleML);
+	}
+	
+	/**
+	 * Test method for {@link org.rulelearn.rules.ruleml.RuleMLBuilder#toRuleMLString(org.rulelearn.rules.Rule)}.
+	 */
+	@Test
+	void testRuleMLBuilderRule1() {
+		Condition<? extends EvaluationField>[] conditions = new Condition<?>[] {this.conditionAtLeast};
+		Condition<? extends EvaluationField>[] decision = new Condition<?>[] {this.decisionAtLeast};
+		
+		when(this.rule.getConditions()).thenReturn(conditions);
+		when(this.rule.getDecisions()).thenReturn(decision);
+		
+		String ruleRuleML = "<assert>\n" + 
+				"\t<implies>\n" + 
+				"\t\t<if>\n" + 
+				"\t\t\t\t<atom>\n" + 
+				"\t\t\t\t<op>\n" + 
+				"\t\t\t\t\t<rel>ge</rel>\n" + 
+				"\t\t\t\t</op>\n" + 
+				"\t\t\t\t<ind>3</ind>\n" + 
+				"\t\t\t\t\t<var>gc1</var>\n" + 
+				"\t\t\t\t</atom>\n" + 
+				"\t\t</if>\n" + 
+				"\t\t<then>\n" + 
+				"\t\t\t\t<atom>\n" + 
+				"\t\t\t\t<op>\n" + 
+				"\t\t\t\t\t<rel>ge</rel>\n" + 
+				"\t\t\t\t</op>\n" + 
+				"\t\t\t\t<ind>b</ind>\n" + 
+				"\t\t\t\t\t<var>gd1</var>\n" + 
+				"\t\t\t\t</atom>\n" + 
+				"\t\t</then>\n" + 
+				"\t</implies>\n" + 
+				"</assert>\n";
+		
+		//System.out.println(ruleMLBuilder.toRuleMLString(this.rule));
+		assertEquals(ruleMLBuilder.toRuleMLString(this.rule), ruleRuleML);
 	}
 
+	/**
+	 * Test method for {@link org.rulelearn.rules.ruleml.RuleMLBuilder#toRuleMLString(org.rulelearn.rules.Rule)}.
+	 */
+	@Test
+	void testRuleMLBuilderRule2() {
+		Condition<? extends EvaluationField>[] conditions = new Condition<?>[] {this.conditionAtMost, this.conditionEqual};
+		Condition<? extends EvaluationField>[] decision = new Condition<?>[] {this.decisionAtMost};
+		
+		when(this.rule.getConditions()).thenReturn(conditions);
+		when(this.rule.getDecisions()).thenReturn(decision);
+		
+		String ruleRuleML = "<assert>\n" + 
+				"\t<implies>\n" + 
+				"\t\t<if>\n" +
+				"\t\t\t<and>\n" +
+				"\t\t\t\t<atom>\n" + 
+				"\t\t\t\t<op>\n" + 
+				"\t\t\t\t\t<rel>le</rel>\n" + 
+				"\t\t\t\t</op>\n" + 
+				"\t\t\t\t<ind>2</ind>\n" + 
+				"\t\t\t\t\t<var>cc1</var>\n" + 
+				"\t\t\t\t</atom>\n" +
+				"\t\t\t\t<atom>\n" + 
+				"\t\t\t\t<op>\n" + 
+				"\t\t\t\t\t<rel>eq</rel>\n" + 
+				"\t\t\t\t</op>\n" + 
+				"\t\t\t\t<ind>1</ind>\n" + 
+				"\t\t\t\t\t<var>a1</var>\n" + 
+				"\t\t\t\t</atom>\n" +
+				"\t\t\t</and>\n" +
+				"\t\t</if>\n" + 
+				"\t\t<then>\n" + 
+				"\t\t\t\t<atom>\n" + 
+				"\t\t\t\t<op>\n" + 
+				"\t\t\t\t\t<rel>le</rel>\n" + 
+				"\t\t\t\t</op>\n" + 
+				"\t\t\t\t<ind>a</ind>\n" + 
+				"\t\t\t\t\t<var>cd1</var>\n" + 
+				"\t\t\t\t</atom>\n" + 
+				"\t\t</then>\n" + 
+				"\t</implies>\n" + 
+				"</assert>\n";
+		
+		//System.out.println(ruleMLBuilder.toRuleMLString(this.rule));
+		assertEquals(ruleMLBuilder.toRuleMLString(this.rule), ruleRuleML);
+	}
 }
