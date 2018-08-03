48ef6076fdfa151adff56bc17d62295c679966bf
Marcin Szelag <marcin.szelag@cs.put.poznan.pl>
2018-08-03 11:54:56 +0200

    DominanceChecker class updated to version from feature/approximations branch. Added tests for DominanceChecker.

---
+++ src/test/java/org/rulelearn/dominance/DominanceCheckerTest.java
@@ -0,0 +1,202 @@
+/**
+ * Copyright (C) Jerzy Błaszczyński, Marcin Szeląg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.rulelearn.dominance;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.rulelearn.data.AttributePreferenceType;
+import org.rulelearn.data.InformationTable;
+import org.rulelearn.data.Table;
+import org.rulelearn.types.EvaluationField;
+import org.rulelearn.types.IntegerField;
+import org.rulelearn.types.IntegerFieldFactory;
+import org.rulelearn.types.RealField;
+import org.rulelearn.types.RealFieldFactory;
+
+/**
+ * Tests for {@link DominanceChecker}.
+ *
+ * @author Jerzy Błaszczyński (<a href="mailto:jurek.blaszczynski@cs.put.poznan.pl">jurek.blaszczynski@cs.put.poznan.pl</a>)
+ * @author Marcin Szeląg (<a href="mailto:marcin.szelag@cs.put.poznan.pl">marcin.szelag@cs.put.poznan.pl</a>)
+ */
+class DominanceCheckerTest {
+	
+	private IntegerField intField(int value, AttributePreferenceType preferenceType) {
+		return IntegerFieldFactory.getInstance().create(value, preferenceType);
+	}
+	
+	private RealField realField(double value, AttributePreferenceType preferenceType) {
+		return RealFieldFactory.getInstance().create(value, preferenceType);
+	}
+	
+	private InformationTable createInformationTableMock(int x, int y, EvaluationField[] xEvaluations, EvaluationField[] yEvaluations) {
+		@SuppressWarnings("unchecked")
+		Table<EvaluationField> evaluations = (Table<EvaluationField>)Mockito.mock(Table.class);
+		
+		Mockito.when(evaluations.getFields(x)).thenReturn(xEvaluations);
+		Mockito.when(evaluations.getFields(y)).thenReturn(yEvaluations);
+		
+		InformationTable informationTableMock = Mockito.mock(InformationTable.class);
+		
+		Mockito.when(informationTableMock.getActiveConditionAttributeFields()).thenReturn(evaluations);
+		
+		return informationTableMock;
+	}
+
+	/**
+	 * Test method for {@link DominanceChecker#dominates(int, int, InformationTable)}.
+	 */
+	@Test
+	void testDominates01() {
+		int x = 0;
+		int y = 1;
+		
+		EvaluationField[] xEvaluations = new EvaluationField[] {intField(2, AttributePreferenceType.GAIN), realField(1, AttributePreferenceType.COST), realField(3, AttributePreferenceType.NONE)};
+		EvaluationField[] yEvaluations = new EvaluationField[] {intField(1, AttributePreferenceType.GAIN), realField(2, AttributePreferenceType.COST), realField(3, AttributePreferenceType.NONE)};
+		
+		assertTrue(DominanceChecker.dominates(x, y, createInformationTableMock(x, y, xEvaluations, yEvaluations)));
+	}
+	
+	/**
+	 * Test method for {@link DominanceChecker#dominates(int, int, InformationTable)}.
+	 */
+	@Test
+	void testDominates02() {
+		int x = 0;
+		int y = 1;
+		
+		EvaluationField[] xEvaluations = new EvaluationField[] {intField(2, AttributePreferenceType.GAIN), realField(1, AttributePreferenceType.COST), realField(3, AttributePreferenceType.NONE)};
+		EvaluationField[] yEvaluations = new EvaluationField[] {intField(2, AttributePreferenceType.GAIN), realField(2, AttributePreferenceType.COST), realField(3, AttributePreferenceType.NONE)};
+		
+		assertTrue(DominanceChecker.dominates(x, y, createInformationTableMock(x, y, xEvaluations, yEvaluations)));
+	}
+	
+	/**
+	 * Test method for {@link DominanceChecker#dominates(int, int, InformationTable)}.
+	 */
+	@Test
+	void testDominates03() {
+		int x = 0;
+		int y = 1;
+		
+		EvaluationField[] xEvaluations = new EvaluationField[] {intField(1, AttributePreferenceType.GAIN), realField(1, AttributePreferenceType.COST), realField(3, AttributePreferenceType.NONE)};
+		EvaluationField[] yEvaluations = new EvaluationField[] {intField(1, AttributePreferenceType.GAIN), realField(1, AttributePreferenceType.COST), realField(2, AttributePreferenceType.NONE)};
+		
+		assertFalse(DominanceChecker.dominates(x, y, createInformationTableMock(x, y, xEvaluations, yEvaluations)));
+	}
+	
+	/**
+	 * Test method for {@link DominanceChecker#dominates(int, int, InformationTable)}.
+	 */
+	@Test
+	void testDominates04() {
+		int x = 0;
+		int y = 1;
+		
+		EvaluationField[] xEvaluations = new EvaluationField[] {intField(1, AttributePreferenceType.GAIN), realField(1, AttributePreferenceType.COST), realField(3, AttributePreferenceType.NONE)};
+		EvaluationField[] yEvaluations = new EvaluationField[] {intField(2, AttributePreferenceType.GAIN), realField(1, AttributePreferenceType.COST), realField(3, AttributePreferenceType.NONE)};
+		
+		assertFalse(DominanceChecker.dominates(x, y, createInformationTableMock(x, y, xEvaluations, yEvaluations)));
+	}
+	
+	/**
+	 * Test method for {@link DominanceChecker#dominates(int, int, InformationTable)}.
+	 */
+	@Test
+	void testDominates05() {
+		int x = 0;
+		int y = 1;
+		
+		EvaluationField[] xEvaluations = new EvaluationField[] {intField(1, AttributePreferenceType.GAIN), realField(2, AttributePreferenceType.COST), realField(3, AttributePreferenceType.NONE)};
+		EvaluationField[] yEvaluations = new EvaluationField[] {intField(1, AttributePreferenceType.GAIN), realField(1, AttributePreferenceType.COST), realField(3, AttributePreferenceType.NONE)};
+		
+		assertFalse(DominanceChecker.dominates(x, y, createInformationTableMock(x, y, xEvaluations, yEvaluations)));
+	}
+
+	/**
+	 * Test method for {@link DominanceChecker#isDominatedBy(int, int, InformationTable)}.
+	 */
+	@Test
+	void testIsDominatedBy01() {
+		int x = 0;
+		int y = 1;
+		
+		EvaluationField[] xEvaluations = new EvaluationField[] {intField(1, AttributePreferenceType.GAIN), realField(2, AttributePreferenceType.COST), realField(3, AttributePreferenceType.NONE)};
+		EvaluationField[] yEvaluations = new EvaluationField[] {intField(2, AttributePreferenceType.GAIN), realField(1, AttributePreferenceType.COST), realField(3, AttributePreferenceType.NONE)};
+		
+		assertTrue(DominanceChecker.isDominatedBy(x, y, createInformationTableMock(x, y, xEvaluations, yEvaluations)));
+	}
+	
+	/**
+	 * Test method for {@link DominanceChecker#isDominatedBy(int, int, InformationTable)}.
+	 */
+	@Test
+	void testIsDominatedBy02() {
+		int x = 0;
+		int y = 1;
+		
+		EvaluationField[] xEvaluations = new EvaluationField[] {intField(1, AttributePreferenceType.GAIN), realField(2, AttributePreferenceType.COST), realField(3, AttributePreferenceType.NONE)};
+		EvaluationField[] yEvaluations = new EvaluationField[] {intField(1, AttributePreferenceType.GAIN), realField(1, AttributePreferenceType.COST), realField(3, AttributePreferenceType.NONE)};
+		
+		assertTrue(DominanceChecker.isDominatedBy(x, y, createInformationTableMock(x, y, xEvaluations, yEvaluations)));
+	}
+	
+	/**
+	 * Test method for {@link DominanceChecker#isDominatedBy(int, int, InformationTable)}.
+	 */
+	@Test
+	void testIsDominatedBy03() {
+		int x = 0;
+		int y = 1;
+		
+		EvaluationField[] xEvaluations = new EvaluationField[] {intField(1, AttributePreferenceType.GAIN), realField(1, AttributePreferenceType.COST), realField(3, AttributePreferenceType.NONE)};
+		EvaluationField[] yEvaluations = new EvaluationField[] {intField(1, AttributePreferenceType.GAIN), realField(1, AttributePreferenceType.COST), realField(2, AttributePreferenceType.NONE)};
+		
+		assertFalse(DominanceChecker.isDominatedBy(x, y, createInformationTableMock(x, y, xEvaluations, yEvaluations)));
+	}
+	
+	/**
+	 * Test method for {@link DominanceChecker#isDominatedBy(int, int, InformationTable)}.
+	 */
+	@Test
+	void testIsDominatedBy04() {
+		int x = 0;
+		int y = 1;
+		
+		EvaluationField[] xEvaluations = new EvaluationField[] {intField(2, AttributePreferenceType.GAIN), realField(1, AttributePreferenceType.COST), realField(3, AttributePreferenceType.NONE)};
+		EvaluationField[] yEvaluations = new EvaluationField[] {intField(1, AttributePreferenceType.GAIN), realField(1, AttributePreferenceType.COST), realField(3, AttributePreferenceType.NONE)};
+		
+		assertFalse(DominanceChecker.isDominatedBy(x, y, createInformationTableMock(x, y, xEvaluations, yEvaluations)));
+	}
+	
+	/**
+	 * Test method for {@link DominanceChecker#isDominatedBy(int, int, InformationTable)}.
+	 */
+	@Test
+	void testIsDominatedBy05() {
+		int x = 0;
+		int y = 1;
+		
+		EvaluationField[] xEvaluations = new EvaluationField[] {intField(1, AttributePreferenceType.GAIN), realField(1, AttributePreferenceType.COST), realField(3, AttributePreferenceType.NONE)};
+		EvaluationField[] yEvaluations = new EvaluationField[] {intField(1, AttributePreferenceType.GAIN), realField(2, AttributePreferenceType.COST), realField(3, AttributePreferenceType.NONE)};
+		
+		assertFalse(DominanceChecker.isDominatedBy(x, y, createInformationTableMock(x, y, xEvaluations, yEvaluations)));
+	}
+
+}
