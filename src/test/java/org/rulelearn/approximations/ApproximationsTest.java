42e757870bab4719c23c2da8daf953449baa9de9
Jurek Blaszczynski <jurek.blaszczynski@cs.put.poznan.pl>
2019-02-25 16:25:02 +0100

    Integration tests from LowerApproximationsTest moved to ApproximationsTest.

--- src/test/java/org/rulelearn/approximations/ApproximationsTest.java
+++ src/test/java/org/rulelearn/approximations/ApproximationsTest.java
@@ -16,21 +16,145 @@
 
 package org.rulelearn.approximations;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.rulelearn.data.Attribute;
+import org.rulelearn.data.InformationTable;
+import org.rulelearn.data.InformationTableWithDecisionDistributions;
+import org.rulelearn.data.csv.ObjectParser;
+import org.rulelearn.data.json.AttributeParser;
+import org.rulelearn.measures.dominance.EpsilonConsistencyMeasure;
 
 /**
- * Integration tests for reading information table from files (attributes + data), calculating unions of decision classes, and calculating their approximations.
- *
+ * Integration tests for reading information table from files (attributes + data) representing ordinal benchmark data sets, calculating unions of decision classes, and calculating their approximations.
+ * 
  * @author Jerzy Błaszczyński (<a href="mailto:jurek.blaszczynski@cs.put.poznan.pl">jurek.blaszczynski@cs.put.poznan.pl</a>)
  * @author Marcin Szeląg (<a href="mailto:marcin.szelag@cs.put.poznan.pl">marcin.szelag@cs.put.poznan.pl</a>)
  */
 @Tag("integration")
 class ApproximationsTest {
 
+	/**
+	 * Test calculation of lower approximations of unions in {@code windsor} data set using {@link ClassicalDominanceBasedRoughSetCalculator}.
+	 */
 	@Test
-	void test() {
-		
+	void testWindsorWithClassicalDominanceBasedRoughSetCalculator() {
+		try (FileReader attributesReader = new FileReader("src/test/resources/data/csv/windsor.json")) {
+			Attribute [] attributes = null;
+			AttributeParser attributeParser = new AttributeParser();
+			attributes = attributeParser.parseAttributes(attributesReader);
+			if (attributes != null) {
+				ObjectParser objectParser = new ObjectParser.Builder(attributes).header(false).separator('\t').build();
+				InformationTable informationTable = null;
+				try (FileReader objectsReader = new FileReader("src/test/resources/data/csv/windsor.csv")) {
+					informationTable = objectParser.parseObjects(objectsReader);
+					if (informationTable != null) {
+						assertEquals(546, informationTable.getNumberOfObjects());
+						// calculate unions
+						Unions unions = new Unions(new InformationTableWithDecisionDistributions(informationTable), new ClassicalDominanceBasedRoughSetCalculator());
+						Union[] downwardUnions = unions.getDownwardUnions(true);
+						Union[] upwardUnions = unions.getUpwardUnions(true);
+						// test downward unions size and lower approximations size 
+						assertEquals(137, downwardUnions[0].size());
+ 						assertEquals(46, downwardUnions[0].getLowerApproximation().size());
+ 						assertEquals(275, downwardUnions[1].size());
+ 						assertEquals(179, downwardUnions[1].getLowerApproximation().size());
+ 						assertEquals(411, downwardUnions[2].size());
+ 						assertEquals(337, downwardUnions[2].getLowerApproximation().size());
+ 						// test upward unions size and lower approximations size 
+						assertEquals(135, upwardUnions[0].size());
+ 						assertEquals(90, upwardUnions[0].getLowerApproximation().size());
+ 						assertEquals(271, upwardUnions[1].size());
+ 						assertEquals(200, upwardUnions[1].getLowerApproximation().size());
+ 						assertEquals(409, upwardUnions[2].size());
+ 						assertEquals(294, upwardUnions[2].getLowerApproximation().size());
+					}
+					else {
+						fail("Unable to load CSV test file with definition of objects");
+					}
+				}
+				catch (FileNotFoundException ex) {
+					System.out.println(ex.toString());
+				}
+				catch (IOException ex) {
+					System.out.println(ex.toString());
+				}
+			}
+			else {
+				fail("Unable to load JSON test file with definition of attributes");
+			}
+		}
+		catch (FileNotFoundException ex) {
+			System.out.println(ex.toString());
+		}
+		catch (IOException ex) {
+			System.out.println(ex.toString());
+		}
+	}
+	
+	/**
+	 * Test calculation of extended (variable consistency) lower approximations of unions in {@code windsor} data set using {@link VCDominanceBasedRoughSetCalculator} and {@link EpsilonConsistencyMeasure}.
+	 */
+	@Test
+	void testWindsorWithVCDominanceBasedRoughSetCalculator() {
+		try (FileReader attributesReader = new FileReader("src/test/resources/data/csv/windsor.json")) {
+			Attribute [] attributes = null;
+			AttributeParser attributeParser = new AttributeParser();
+			attributes = attributeParser.parseAttributes(attributesReader);
+			if (attributes != null) {
+				ObjectParser objectParser = new ObjectParser.Builder(attributes).header(false).separator('\t').build();
+				InformationTable informationTable = null;
+				try (FileReader objectsReader = new FileReader("src/test/resources/data/csv/windsor.csv")) {
+					informationTable = objectParser.parseObjects(objectsReader);
+					if (informationTable != null) {
+						assertEquals(546, informationTable.getNumberOfObjects());
+						// calculate unions
+						Unions unions = new Unions(new InformationTableWithDecisionDistributions(informationTable), new VCDominanceBasedRoughSetCalculator(EpsilonConsistencyMeasure.getInstance(), 0.1));
+						Union[] downwardUnions = unions.getDownwardUnions(true);
+						Union[] upwardUnions = unions.getUpwardUnions(true);
+						// test downward unions size and lower approximations size 
+						assertEquals(137, downwardUnions[0].size());
+ 						assertEquals(134, downwardUnions[0].getLowerApproximation().size());
+ 						assertEquals(275, downwardUnions[1].size());
+ 						assertEquals(275, downwardUnions[1].getLowerApproximation().size());
+ 						assertEquals(411, downwardUnions[2].size());
+ 						assertEquals(409, downwardUnions[2].getLowerApproximation().size());
+ 						// test upward unions size and lower approximations size 
+						assertEquals(135, upwardUnions[0].size());
+ 						assertEquals(135, upwardUnions[0].getLowerApproximation().size());
+ 						assertEquals(271, upwardUnions[1].size());
+ 						assertEquals(268, upwardUnions[1].getLowerApproximation().size());
+ 						assertEquals(409, upwardUnions[2].size());
+ 						assertEquals(391, upwardUnions[2].getLowerApproximation().size());
+					}
+					else {
+						fail("Unable to load CSV test file with definition of objects");
+					}
+				}
+				catch (FileNotFoundException ex) {
+					System.out.println(ex.toString());
+				}
+				catch (IOException ex) {
+					System.out.println(ex.toString());
+				}
+			}
+			else {
+				fail("Unable to load JSON test file with definition of attributes");
+			}
+		}
+		catch (FileNotFoundException ex) {
+			System.out.println(ex.toString());
+		}
+		catch (IOException ex) {
+			System.out.println(ex.toString());
+		}
 	}
 
 }
